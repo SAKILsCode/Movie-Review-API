37828c779bdf365c98c8219e233f755313d4489a
MD Sakil Hasan <mdsakilhasan833@gmail.com>
2023-09-10 15:01:16 +0600

    🚧 [ADD] middlewares

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,11 +1,21 @@
+/**
+ * Title: Middleware index
+ * Description: Middleware entry point, contains all global middlewares.
+ */
+
+// Dependencies
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
+/**
+ * Middleware Serving function
+ * @param {Object} app
+ */
 const middleware = (app) => {
   app.use(cors());
   app.use(morgan('dev'));
   app.use(express.json());
 };
 
-module.exports = middleware
+module.exports = middleware;
