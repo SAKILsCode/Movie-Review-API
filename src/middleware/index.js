ea0691a18d5f9e0b9d26ac6c3d50622fbd9c6392
MD Sakil Hasan <mdsakilhasan833@gmail.com>
2023-09-10 15:04:49 +0600

    📝 [UPDATE] setup Swagger doc & OpenApiValidator

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -8,6 +8,13 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
+const YAML = require('yamljs');
+const swaggerUI = require('swagger-ui-express');
+const OpenApiValidator = require('express-openapi-validator');
+
+// Loading swagger doc
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 /**
  * Middleware Serving function
  * @param {Object} app
@@ -16,6 +23,16 @@ const middleware = (app) => {
   app.use(cors());
   app.use(morgan('dev'));
   app.use(express.json());
+
+  // Serving swagger document to "/doc" route
+  app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+  // Validation using swagger.yaml
+  app.use(
+    OpenApiValidator.middleware({
+      apiSpec: './swagger.yaml',
+    })
+  );
 };
 
 module.exports = middleware;
