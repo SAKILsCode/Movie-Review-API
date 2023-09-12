2533accea2425520e8347df4ee6e9add4d9f072f
MD Sakil Hasan <mdsakilhasan833@gmail.com>
2023-09-12 18:22:23 +0600

    💡 code comments

--- src/model/index.js
+++ src/model/index.js
@@ -1,3 +1,8 @@
+/**
+ * Title: Schema Models
+ * Description: contains models of all available entities in DB, these model are use to make operation on DB.
+ */
+
 const User = require('./User')
 const Movie = require('./Movie')
 const Review = require('./Review')
