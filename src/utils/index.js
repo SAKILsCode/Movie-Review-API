9cc9df029d76571a541e02338fe1f0a6c4824590
MD Sakil Hasan <mdsakilhasan833@gmail.com>
2023-09-23 00:17:03 +0600

    🔨 [ADD] Hashing functions

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,11 @@
 const query = require('./query')
 const error = require('./error')
+const hashing = require('./hashing')
+const matchLoggedInUser = require('./matchLoggedInUser')
 
 module.exports = {
   query,
-  error
+  error,
+  hashing,
+  matchLoggedInUser
 }
