00852f1f685da8f901142c1840a3e96c3c932148
MD Sakil Hasan <mdsakilhasan833@gmail.com>
2023-10-25 06:41:17 +0600

    ⚰️ [REMOVED] unused variable 'matchLoggedInUser' removed

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,9 @@
 const query = require('./query')
 const error = require('./error')
 const hashing = require('./hashing')
-const matchLoggedInUser = require('./matchLoggedInUser')
 
 module.exports = {
   query,
   error,
-  hashing,
-  matchLoggedInUser
+  hashing
 }
